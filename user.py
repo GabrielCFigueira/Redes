66ed5f7b981a41e0acc671df2202fe54dfba2c13
LivioCosta <43416865+LivioCosta@users.noreply.github.com>
2018-09-22 13:33:46 +0100

    Update login(AUT)

--- user.py
+++ user.py
@@ -3,17 +3,22 @@
 import sys
 import socket
 
-flagCSname = sys.argv[1]
-CSname = sys.argv[2]
-flagCSport = sys.argv[3]
-CSport = 58000+eval(sys.argv[4])
+CSname = socket.gethostname()
+CSport = 58037
+
+if len(sys.argv)==3:
+    flagCSname = sys.argv[1]
+    CSname = sys.argv[2]
+elif len(sys.argv)==5:
+    flagCSport = sys.argv[3]
+    CSport = eval(sys.argv[4])
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def printar(arg1, arg2, arg3, arg4):
-    if(arg1=="-n"):
+    if arg1=="-n":
         print(arg2)
-    if(arg3=="-p"):
+    if arg3=="-p":
         print(arg4)
 
 def connect_TCP():
@@ -24,7 +29,13 @@ def send_message(message):
 
 def receive_message():
     response = client.recv(4096)
-    print(response.decode(), end='')
+    response_list = response.decode().split()
+    if response_list[0]=="ERR":
+        err_messages(response_list[1])
+
+def err_messages(flag):
+    if flag=="AUT":
+        print("Para se autenticar tem que executar: login user pass")
 
 def read_command():
     command = ""
@@ -33,9 +44,12 @@ def read_command():
         commands = command.split()
 
         if commands[0]=="login":
-            connect_TCP()
-            send_message(commands[1]+" "+commands[2]+"\n")
-            receive_message()
+            if len(commands)==3:
+                err_messages("AUT")
+            else:
+                connect_TCP()
+                send_message("AUT "+commands[1]+" "+commands[2]+"\n")
+                receive_message()
 
 
 read_command()
