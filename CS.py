e4c60c643e01cff91fa21b91a9d027cda8df51ee
Grabiel14 <33735410+Grabiel14@users.noreply.github.com>
2018-09-23 17:58:15 +0100

    Started working on pipes in CS server

--- CS.py
+++ CS.py
@@ -79,15 +79,47 @@ if len(sys.argv) == 3 and sys.argv[1] == "-p":
 elif len(sys.argv) > 1:
     raise ValueError("numero errado de argumentos")
     exit()
+
+
+
+
+if os.fork() == 0:
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket.bind(('', CSPort))
+    while True:
+
+        bytesAddressPair = UDPServerSocket.recvfrom(1024)
+
+        message = bytesAddressPair[0].decode()
+
+        address = bytesAddressPair[1]
+
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP  = "Client IP Address:{}".format(address)
+
+        print(clientMsg)
+        print(clientIP)
+
+        # Sending a reply to client
+
+        #UDPServerSocket.sendto(bytesToSend, address)
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', CSPort))
 server.listen(20)  # max backlog of connections
 
+PipeList = []
+leitorEscravo = 0
+escritorMestre = 0
+
+
 
 while True:
     client_socket, address = server.accept()
-    pid = os.fork()
-    if pid == 0:
+    leitorEscravo, escritorMestre = os.pipe()
+    PipeList += [escritorMestre]
+    if os.fork() == 0:
+
         print('Accepted connection from {}:{}'.format(address[0], address[1]))
         connectionClosed = False
         while not connectionClosed:
