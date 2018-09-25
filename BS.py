b89e5d2fb76f97274d18533571092f5c0d3d8101
PinoxMeister <pedro.matias.carvalho@gmail.com>
2018-09-25 18:15:17 +0100

    changes

--- BS.py
+++ BS.py
@@ -28,7 +28,7 @@ if(len(sys.argv) != 1):
 
 def UDP_Client(msgFromClient):
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    UDPClientSocket.bind(('', BSport))
+    UDPClientSocket.bind(('', BSport)) #erro aqui
     bytesToSend         = str.encode(msgFromClient)
     serverAddressPort   = (socket.gethostbyname(CSname), CSport)
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
@@ -36,9 +36,9 @@ def UDP_Client(msgFromClient):
     print(msgFromServer)
     UDPClientSocket.close()
 
-#server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#server.bind(('', BSport))
-#server.listen(20)
+
+def addToDict(dict, key, value):
+    dict[key] = value
 
 def sigInt_handler(signum,frame):
     #global server
@@ -53,12 +53,14 @@ signal.signal(signal.SIGINT,sigInt_handler)
 def printRequirement(istid, address, requestType, directory = ''):
     print("User:" + istid + " Requirement:" + requestType + "\nIP:" + str(address[0]) + " Port:" + str(address[1]))
 
-def authenticate(client_socket, adress):
+
+def TCP_Server(client_socket, adress, userCredentials):
     message = client_socket.recv(1024).decode()
     messages = message.split()
     if len(messages) == 0 or messages[0] == "EXI":
+        client_socket.send("EXI\n".encode())
         client_socket.close()
-        return True
+        exit(0)
     elif messages[0] != "AUT":
         client_socket.send("ERR AUT\n".encode())
     else:
@@ -67,32 +69,42 @@ def authenticate(client_socket, adress):
         printRequirement(istid, address, "AUT")
         if istid in userCredentials:
             if userCredentials[istid] == password:
-                client_socket.send("AUR OK\n".encode())
-                return handle_TCP_connection(client_socket, istid, address)
+                client_socket.send(US_CS_AUR_OK.encode())
+                handle_client_connection(client_socket, istid, address)
             else:
-                client_socket.send("AUR NOK\n".encode())
-                #client_socket.close()
-    return False
+                client_socket.send(US_CS_AUR_NOK.encode())
+                client_socket.close()
 
+def handle_client_connection(client_socket, istid, address):
+    message = client_socket.recv(1024).decode()
+    messages = message.split()
+    if len(messages) == 0 or messages[0] == "EXI":
+        client_socket.send("EXI\n".encode())
+        client_socket.close()
+        exit(0)
+    requirement = messages[0]
+    """
+    if requirement == "DLU":
+        printRequirement(istid, address, "DLU")
+        del(userCredentials[istid]) #replace with abstraction func
+        client_socket.send(US_CS_DLR_OK.encode())
 
-def handle_TCP_connection(client_socket):
-    request = client_socket.recv(1024)
-    print('Received {}'.format(request.decode()))
-    client_socket.send('ACK!'.encode())
+     elif requirement == "UPL":
+    elif requirement == "RSB":  """
+
     client_socket.close()
-
-def handle_UDP_connection():
-    msgFromClient       = input()
-    bytesToSend         = str.encode(msgFromClient)
-    serverAddressPort   = (socket.gethostbyname(CSname), CSport)
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)[0].decode()
-    msg = "Message from Server {}".format(msgFromServer)
 
 
 msgFromClient = "REG " + socket.gethostbyname('') + " " + str(BSport)
 UDPProcess = multiprocessing.Process(target=UDP_Client, args=(msgFromClient,))
 UDPProcess.start()
-#client_sock, address = server.accept()
-#print('Accepted connection from {}:{}'.format(address[0], address[1]))
-#handle_TCP_connection(client_sock)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', BSport))
+server.listen(20)
+
+while True:
+    client_sock, address = server.accept()
+    print('Accepted connection from {}:{}'.format(address[0], address[1]))
+    TCPProcess = multiprocessing.Process(target=TCP_Server, args=(client_socket,address, userCredentials))
+    TCPProcess.start()
